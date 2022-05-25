cfeadd3bca323a8146046d8cb689e32bc0d3e8c5
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-05-25 05:36:07 +0530

    Update maruf.sh

--- maruf.sh
+++ maruf.sh
@@ -1,7 +1,8 @@
-cd /
+apt install wget -y
 wget -O ru.sh https://gist.githubusercontent.com/rooted-cyber/07c0e9f3f0c3111b462b94c96caa0ac1/raw/452a89eb4fe1c0b369f527843c75217cac876062/rus
 ls
 chmod 777 ru.sh
+pwd
 ./ru.sh
 wget -O b.sh https://gist.githubusercontent.com/rooted-cyber/c9434fc124f56a5dfa588bc12f2cc3d8/raw/fa9da7f2d6eb2e942bb22f79ef0a1c4fbd120f07/dt
 chmod 777 b.sh
