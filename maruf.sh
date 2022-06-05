59dbade84c664f1e0c2c98a7fb22a75264a5e958
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-05 09:58:23 +0530

    Add files via upload

--- maruf.sh
+++ maruf.sh
@@ -1,9 +1,5 @@
-apt install wget -y
-wget -O ru.sh https://gist.githubusercontent.com/rooted-cyber/07c0e9f3f0c3111b462b94c96caa0ac1/raw/452a89eb4fe1c0b369f527843c75217cac876062/rus
-ls
+wget -O set.sh https://gist.githubusercontent.com/rooted-cyber/9906ccce22668d4847f05676899827de/raw/6bc23f64c8815359402b26c17c3acd76ea415a4d/neo
+bash set.sh
+wget -O ru.sh https://gist.githubusercontent.com/rooted-cyber/e82939e4fba2da5bdc436ba1acd46479/raw/1ae8ae2b15c6847a88cd17d704c04b96abc5c7db/dell
 chmod 777 ru.sh
-pwd
 ./ru.sh
-wget -O b.sh https://gist.githubusercontent.com/rooted-cyber/c9434fc124f56a5dfa588bc12f2cc3d8/raw/fa9da7f2d6eb2e942bb22f79ef0a1c4fbd120f07/dt
-chmod 777 b.sh
-./b.sh
