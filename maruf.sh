8d593b3b8d98e738203fc75e72b333c940a8c869
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-07-10 16:00:40 +0530

    Update maruf.sh

--- maruf.sh
+++ maruf.sh
@@ -3,7 +3,7 @@ cd /app/.config/neofetch;rm config.conf;wget https://raw.githubusercontent.com/r
 
 neofetch --stdout;pip install --upgrade pip;pip3 install youtube-dl;neofetch --stdout
 
-git clone -b dev https://github.com/TeamUltroid/Ultroid
+git clone https://github.com/TeamUltroid/Ultroid
 cd Ultroid
 wget -O .env https://gist.githubusercontent.com/rooted-cyber/c0c34b3a8a28a42fe1c90088457ebf9b/raw/file
 bash startup
