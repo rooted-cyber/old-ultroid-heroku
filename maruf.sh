03e372ee61ce6d6b467eefb6b34dcfe24035657e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-07-10 14:40:28 +0530

    Add files via upload

--- maruf.sh
+++ maruf.sh
@@ -1,12 +1,9 @@
-wget -O apt.sh https://gist.githubusercontent.com/rooted-cyber/c433d471542c4cfb27fec1b100a0f599/raw/083946b4330d603add38c9bee83b0d0a40e4a50d/apt
-chmod 777 apt.sh
-./apt.sh
-wget -O a.sh https://gist.githubusercontent.com/rooted-cyber/07c0e9f3f0c3111b462b94c96caa0ac1/raw/452a89eb4fe1c0b369f527843c75217cac876062/rus
-chmod 777 a.sh
-./a.sh
-wget -O set.sh https://gist.githubusercontent.com/rooted-cyber/9906ccce22668d4847f05676899827de/raw/6bc23f64c8815359402b26c17c3acd76ea415a4d/neo
-chmod 777 set.sh
-./set.sh
-wget -O ru.sh https://gist.githubusercontent.com/rooted-cyber/8bdb757841e90b4bcc72ae72714e77b3/raw/b904c0786f2809c2048ca3f98ee9995bce91847f/ultdep
-chmod 777 ru.sh
-./ru.sh
+neofetch
+cd /app/.config/neofetch;rm config.conf;wget https://raw.githubusercontent.com/rooted-cyber/uploading/main/c1/config.conf
+
+neofetch --stdout;pip install --upgrade pip;pip3 install youtube-dl;neofetch --stdout
+
+git clone -b dev https://github.com/TeamUltroid/Ultroid
+cd Ultroid
+wget -O .env https://gist.githubusercontent.com/rooted-cyber/c0c34b3a8a28a42fe1c90088457ebf9b/raw/b2225dbc982149780fdd1fe1a0ba8b570023a9de/file
+bash startup
